b116c539582a8174fde99fae6d60ff6f6726f47b
pkb1981 <pallab.b7@gmail.com>
2026-03-03 21:34:13 +0600

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,26 @@ function App() {
   return (
     <>
 
-      <h1>Welcome to Assignment 7</h1>
+      {/* navbar section */}
+      <section>
+        <div class="navbar w-11/12 mx-auto bg-base-100 shadow-sm">
+          <div class="flex-1">
+            <a class="btn btn-ghost text-xl">CS — Ticket System</a>
+          </div>
+          <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+              <li><a>Home</a></li>
+              <li><a>FAQ</a></li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+              <li><button class="bg-[#9F62F2] text-[#FFFFFF]">+ New Ticket</button></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
 
     </>
   )
