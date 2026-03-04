1f0fe8e0167f706cb43e9cd0e3ff056ce4dca463
pkb1981 <pallab.b7@gmail.com>
2026-03-04 22:09:45 +0600

    api completed

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,34 +9,38 @@ const Main = ({ ticketsPromise }) => {
         <div className="mx-auto w-11/12 mt-4">
             <h3 className="font-medium text-2xl my-4">Customer Tickets</h3>
 
-            {/* small card from daisy ui */}
-            <div>
-                <div className="card w-96 bg-base-100 card-xs shadow-sm">
 
-                    <div className="card-body">
+            <div className="grid grid-cols-2 w-2/3 gap-8">
+                {/* small card from daisy ui */}
+                {
 
-                        <div className="flex justify-between">
-                            <h2 className="card-title text-[14px]">Login Issues - Can't Access Account</h2>
-                            <button className="flex items-center bg-[#B9F8CF] rounded-xl px-2 text-[#0B5E06] font-medium text-[14px]"><GoDotFill size={25} />Open</button>
-                        </div>
-
-                        <p className="text-[13px]">Customer is unable to log in to their account. They've tried resetting their password multiple times but still...</p>
-                        <div className="justify-between card-actions">
-                            <div className="flex space-x-2.5">
-                                <h4>#1001</h4>
-                                <h4>HIGH PRIORITY</h4>
+                    allTickets.map(ticket => <div className="card w-96 bg-base-100 card-xs shadow-sm">
+                        <div className="card-body">
+                            <div className="flex justify-between">
+                                <h2 className="card-title text-[14px]">{ticket.title}</h2>
+                                <button className=" btn btn-sm flex items-center bg-[#B9F8CF] rounded-3xl px-2 text-[#0B5E06] text-[14px] font-medium "><GoDotFill size={25} />{ticket.status}</button>
                             </div>
-                            <div className="flex space-x-2.5">
-                                <h4>John Smith</h4>
-                                <div className="flex items-center">
-                                    <CiCalendarDate />
-                                    <h4>1/15/2024</h4>
+
+                            <p className="text-[13px]">{ticket.description}</p>
+                            <div className="justify-between card-actions">
+                                <div className="flex space-x-2.5">
+                                    <span>#{ticket.id}</span>
+                                    <span>{ticket.priority
+                                    } PRIORITY</span>
+                                </div>
+                                <div className="flex space-x-2.5">
+                                    <span>{ticket.customer}</span>
+                                    <div className="flex items-center">
+                                        <CiCalendarDate />
+                                        <span>{ticket.createdAt}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </div>)
+                }
             </div>
+
         </div>
     );
 };
