96737a5494fc8805e77d46ed9a43b11bf48fac3c
pkb1981 <pallab.b7@gmail.com>
2026-03-05 07:34:43 +0600

    all ticket api

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -10,26 +10,45 @@ const Main = ({ ticketsPromise }) => {
             <h3 className="font-medium text-2xl my-4">Customer Tickets</h3>
 
 
-            <div className="grid grid-cols-2 w-2/3 gap-8">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 w-2/3 gap-8">
                 {/* small card from daisy ui */}
                 {
 
-                    allTickets.map(ticket => <div className="card w-96 bg-base-100 card-xs shadow-sm">
+                    allTickets.map(ticket => <div className="card bg-base-100 card-xs shadow-sm">
                         <div className="card-body">
                             <div className="flex justify-between">
-                                <h2 className="card-title text-[14px]">{ticket.title}</h2>
-                                <button className=" btn btn-sm flex items-center bg-[#B9F8CF] rounded-3xl px-2 text-[#0B5E06] text-[14px] font-medium "><GoDotFill size={25} />{ticket.status}</button>
+                                <h2 className="card-title font-bold text-[14px]">{ticket.title}</h2>
+
+                                <button
+                                    className={`btn btn-sm flex items-center rounded-3xl px-2 text-[14px] font-medium
+                                        ${ticket.status?.toLowerCase() === "open"
+                                            ? "bg-[#B9F8CF] text-[#0B5E06]"
+                                            : ticket.status?.toLowerCase() === "in progress"
+                                                ? "bg-yellow-200 text-yellow-800"
+                                                : "bg-gray-200 text-gray-700"
+                                        }`}
+                                >
+                                    <GoDotFill size={25} />
+                                    {ticket.status}
+                                </button>
                             </div>
 
                             <p className="text-[13px]">{ticket.description}</p>
                             <div className="justify-between card-actions">
-                                <div className="flex space-x-2.5">
+                                <div className="flex space-x-2.5 font-bold">
                                     <span>#{ticket.id}</span>
-                                    <span>{ticket.priority
-                                    } PRIORITY</span>
+                                    <span className={
+                                        ticket.priority === "LOW" ? "text-[#02A53B]" :
+                                            ticket.priority === "MEDIUM" ? "text-[#FEBB0C]" :
+                                                ticket.priority === "HIGH" ? "text-[#F83044]" :
+                                                    ticket.priority === "CRITICAL" ? "text-red-800" :
+                                                        ""
+                                    }>
+                                        {ticket.priority} PRIORITY
+                                    </span>
                                 </div>
                                 <div className="flex space-x-2.5">
-                                    <span>{ticket.customer}</span>
+                                    <span className="font-bold text-xs">{ticket.customer}</span>
                                     <div className="flex items-center">
                                         <CiCalendarDate />
                                         <span>{ticket.createdAt}</span>
@@ -41,7 +60,7 @@ const Main = ({ ticketsPromise }) => {
                 }
             </div>
 
-        </div>
+        </div >
     );
 };
 
